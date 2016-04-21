1e169711680a208416961d4487dba8bfed5f6212
nscalf561 <scalf.nicholas@gmail.com>
2016-04-21 12:35:13 -0700

    inital set up, route architecture

---
+++ config/routes.js
@@ -0,0 +1,78 @@
+var express 		= require('express'),
+		router			= express.Router(),
+		path 				= require('path'),
+		app 				= express(),
+		bodyParser  = require('body-parser'),
+		hbs					= require('hbs');
+
+// Home route/landing page
+router.route('/')
+	.get(viewController.home);
+
+/************
+	ABOUT
+	**********/
+
+router.route('/aboutGavin')
+	.get(viewController.aboutGavin);
+
+router.route('/aboutJennifer')
+	.get(viewController.aboutJennifer);
+
+/**********
+	LATEST
+ *********/
+
+// Latest page, it's a feed of news
+router.route('/latest')
+	.get(viewController.latest);
+
+router.route('/latest/press-releases')
+	.get(viewController.pressReleases);
+
+/*************
+	ISSUES
+ *************/
+
+router.route('/issues')
+	.get(viewController.issues);
+
+router.route('/issues/economy')
+	.get(viewController.economy);
+
+router.route('/issues/energy')
+	.get(viewController.energy);
+
+router.route('/issues/civil-rights')
+	.get(viewController.civilRights);
+
+router.route('/issues/schools')
+	.get(viewController.schools);
+
+router.route('/issues/drug-policy')
+	.get(viewController.drugPolicy);
+
+router.route('/issues/government')
+	.get(viewController.government);
+
+/**************
+	GET INVOLVED
+ *************/
+
+router.route('/getInvolved')
+	.get(viewController.getInvolved);
+
+router.route('/getInvolved/endorseGavin')
+	.get(viewController.endorseGavin);
+
+router.route('/getInvolved/volunteer')
+	.get(viewController.volunteer);
+
+router.route('/getInvolved/register')
+	.get(viewController.register);
+
+// Privacy Policy
+router.route('/privacy-policy')
+	.get(viewController.privacyPolicy);
+
+	
---
+++ server.js
@@ -0,0 +1,26 @@
+var express 		= require('express'),
+		app 				= express(),
+		bodyParser 	= require('body-parser');
+
+// Allows for cross origin resource sharing, this needs to be changed
+// for deployment --- settings are not secure right now
+app.use(cors());
+
+// Configures boydParser (accepts form data)
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+//TODO DO WE NEED THIS?
+// Serves static files from public folder
+app.use(express.static(__dirname + '/public'));
+
+// Sets view engine to hbs
+app.set('view engine', 'hbs');
+
+// This grabs the file we're making the routes from
+app.use(routes);
+
+//TODO: We'll reconfigure this outside of development
+app.listen(3000, function() {
+	console.log('server is up');
+});
