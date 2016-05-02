d3608250ece6d3e1d60ba64616f4e8435925157f
nscalf561 <scalf.nicholas@gmail.com>
2016-05-01 18:30:01 -0700

    FIXES THE RELOAD ISSUE! States persist on reload

--- frontend/media/js/app.js
+++ frontend/media/js/app.js
@@ -1,65 +1,62 @@
 var App = angular.module('starter', [
-														'index.controller',
 														'ui.router'
 													]);
 
-App.config(['$stateProvider', '$urlRouterProvider', '$locationProvider', function($stateProvider, $urlRouterProvider, $locationProvider) {
+App.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
 
 		// if none of the above states are matched, use this as the fallback
-		// $urlRouterProvider.otherwise('/app');
+		$urlRouterProvider.otherwise("/");
 
 		$stateProvider
 
 			.state('app', {
 				url: '/',
 				templateUrl: './templates/index.html',
-				// controller: 'IndexCtrl'
 			})
 
 			.state('aboutGavin', {
-				url: 'about-Gavin',
+				url: '/about-Gavin',
 				templateUrl: './templates/about/aboutGavin.html',
 			})
 
 			.state('aboutJennifer', {
-				url:'about-Jennifer',
+				url:'/about-Jennifer',
 				templateUrl: './templates/about/aboutJennifer.html'
 			})
 
 			.state('contact', {
-				url: 'contact',
+				url: '/contact',
 				templateUrl: './templates/contact.html'
 			})
 
 			.state('endorse', {
-				url: 'endorse',
+				url: '/endorse',
 				templateUrl: './templates/endorse.html'
 			})
 
 			.state('get-involved', {
-				url: 'get-involved',
+				url: '/get-involved',
 				templateUrl: './templates/getInvolved.html'
 			})
 
 			.state('issues', {
-				url: 'issues',
+				url: '/issues',
 				templateUrl: './templates/issues.html'
 			})
 
 			.state('latest-news', {
-				url: 'latest-news',
+				url: '/latest-news',
 				templateUrl: './templates/latestNews.html'
 			})
 
 			.state('volunteer', {
-				url: 'volunteer',
+				url: '/volunteer',
 				templateUrl: './templates/volunteer.html'
 			});
 
-		$locationProvider.html5Mode({
-			enabled: true,
-			requiredBase: false
-		});
-
+		// $locationProvider.html5Mode({
+		// 	enabled: true,
+		// 	requiredBase: false
+		// });
 
 	}]);
